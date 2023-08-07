ac55172f983a2bb3f8820679410291b13522c2a8
Rajneesh-S <140791190+Rajneesh-S@users.noreply.github.com>
2023-08-07 11:11:18 +0530

    added user.js and creategroup.js

--- user.js
+++ user.js
@@ -4,11 +4,13 @@ export default class User{
         this.name = name;
         this.#password = password;
         this.expenses = [];
-        this.groups= [];
+        this.group= [];
         this.owes= [];
     }
 
-
+    get password(){
+        return this.#password;
+    }
 
     authentication(username,password)
     {
@@ -20,16 +22,43 @@ export default class User{
         this.expenses.push(expense);
     }
 
-    addgroup(group)
+    addGroupMember(groupMember)
     {
-        this.group.push(group);
+        this.group.push(groupMember);
     }
+
+    getGroupMember(memberName){
+        let index = this.group.findIndex(member => member.name === memberName);
+        try{
+            if(index === -1) throw 'User not in your group, First Add user to your group'
+            return this.group[index];
+        }
+        catch(err){
+            console.log(err);
+            return false;
+        }
+        
+    }
+
     addowes(owes)
     {
         this.owes.push(owes);
     }
 
-    
+    findExpense(expenseName){
+        return this.expenses.find(expense => expense.name === expenseName) || null;
+    }
 
+    existGroupMember(name){
+        return this.group.find(groupMember => groupMember.name === name) || null;
+    }
+
+    existExpenseName(expenseName){
+        return this.expenses.find(expense => expense.name === expenseName) || null;
+    }
+
+    toString(){
+        return this.name;
+    }
 
 }
